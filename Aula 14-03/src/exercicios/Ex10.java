7abccb1cb8b64d1fb4092ce37c1a40bd33103fd4
Lucas Emanoel Gitirana <gitiranalucas5@gmail.com>
2023-03-21 21:05:06 -0300

    Update Ex10.java

--- Aula 14-03/src/exercicios/Ex10.java
+++ Aula 14-03/src/exercicios/Ex10.java
@@ -1,11 +1,13 @@
 package lista01_condicionais;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ex10 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("R$ ######0.00");
 		
 		System.out.println("Peso dos peixes: ");
 		double peso = s.nextDouble();
@@ -18,7 +20,7 @@ public class Ex10 {
 			excesso = peso - 500;
 			multa = excesso * 4;
 			mensagem = "Excesso de peso: "+excesso+" kg\n"
-					+"Multa: R$ "+multa;
+					+"Multa: R$ "+df.format(multa);
 		}else {
 			mensagem = "Não há multa por excesso de peso.";
 		}
