dbdbe40023d8fa347d036750720a11a437a88552
Lucas Emanoel Gitirana <gitiranalucas5@gmail.com>
2023-04-11 21:56:12 -0300

    Add files via upload

--- ListaWhile/src/lista/Ex07.java
+++ ListaWhile/src/lista/Ex07.java
@@ -7,8 +7,15 @@ public class Ex07 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		System.out.println("Número: ");
-		int num = s.nextInt();
+		int num = 0;
+		while(num < 1000 || num > 9999) {
+			System.out.println("Número: ");
+			num = s.nextInt();
+			
+			if (num < 1000 || num > 9999) {
+				System.out.println("Valor inválido. Digite um número entre 1000 e 9999");
+			}
+		}			
 		
 		int situacao1 = (num / 100) + (num % 100);
 		int situacao2 = (int) Math.pow(situacao1, 2);
