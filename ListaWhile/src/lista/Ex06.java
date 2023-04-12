3b7fc48382c1923de30fb056e39303ff76ed6976
Lucas Emanoel Gitirana <gitiranalucas5@gmail.com>
2023-04-11 21:48:59 -0300

    Add files via upload

--- ListaWhile/src/lista/Ex06.java
+++ ListaWhile/src/lista/Ex06.java
@@ -6,21 +6,28 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Número: ");
-		int num = s.nextInt();
 		
-		int x = 1;
-		int subtracao = num;
-		int resposta = 0;
-		while(subtracao > 0) {
-			subtracao = subtracao - x; 
-			if (subtracao >= 0) {
-				resposta++;				
+		String continuar = "s";
+		while(continuar.equalsIgnoreCase("s")) {			
+			System.out.println("Número: ");
+			int num = s.nextInt();
+			
+			int x = 1;
+			int subtracao = num;
+			int resposta = 0;
+			while(subtracao > 0) {
+				subtracao = subtracao - x; 
+				resposta++;
+				x+=2;
 			}
-			x+=2;
+			
+			System.out.println("Resposta: "+resposta);
+			
+			System.out.println("Continuar? ");
+			continuar = s.next();
 		}
 		
-		System.out.println("Resposta: "+resposta);
+		
 
 	}
 
