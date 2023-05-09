44916109b5589aa9670c9d7826604735e02de2ff
Lucas Emanoel Gitirana <gitiranalucas5@gmail.com>
2023-05-09 20:11:08 -0300

    Update Ex01.java

--- Vetores02/src/listaVetores02/Ex01.java
+++ Vetores02/src/listaVetores02/Ex01.java
@@ -3,35 +3,37 @@ package listaVetores02;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		int v[] = new int[10];
+		int v[] = new int[150];
 
 		int num = 1;
 		for (int i=0;i<v.length;i++) {
 			v[i] = num;
-			
-			int divisor = num;
 			String binario = "";
-			while(divisor >= 1) {				
-				if(divisor == 1) {
-					binario = 1 +" "+binario;
+			
+			if(v[i] <= 255) {
+				int divisor = num;				
+				while(divisor >= 2) {								
+					binario = (divisor % 2) + binario;			
+					divisor = divisor / 2;						
 				}
-				else {
-					binario = (divisor % 2) +" "+ binario;
-				}				
-				divisor = divisor / 2;						
+				binario = (divisor % 2) + binario;
+				
+				while(binario.length() < 8) {
+					binario = 0 + binario;
+				}
+			}else {
+				binario = "XXXXXXXX";
 			}
 			
-			System.out.println("Valor informado: "+v[i]);
+			
+			System.out.println(binario+" = "+v[i]);
+			/*System.out.println("Valor informado: "+v[i]);
 			System.out.println("Correspondente em binário: "+binario);
-			System.out.println("======================================");
+			System.out.println("Tamanho da string: "+binario.length());
+			System.out.println("======================================");*/
 			
 			num += 2;
 		}
-		
-		for(int i = 0; i<v.length; i++) {
-			System.out.println(v[i]);
-		}
-
 	}
 
 }
